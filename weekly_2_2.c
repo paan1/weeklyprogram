f8f596d3374f2b2b13fa2f94cd185c82a512c52c
patiphan01 <86480648+paan1@users.noreply.github.com>
2021-08-22 00:41:36 +0700

    .

--- weekly_2_2.c
+++ weekly_2_2.c
@@ -1,24 +1,28 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int n;
     printf("Enter number:");
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for (int i = 1; i <= n; i++)
     {
-     for(j=1;j<=n;j++)
-     {
-         if(i==1 || j == 1 || i ==n || j == n)
-         {
-             printf("*");
-         }
-         else
-         {
-             printf(" ");
-         }
-         
-     }
-     printf("\n");
+        printf("*");
+    }
+    printf("\n");
+    for (int down = 1; down <= (n-2); down++)
+    {
+    printf("*");
+    for (int space = 1; space <= (n-2); space++)
+    {
+        printf(" ");
+    }
+    printf("*");
+    printf("\n");
+    }
+    for(int i=1; i <= n; i++)
+    {
+        printf("*");
     }
     return 0;
+    
 }
