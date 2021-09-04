7f5fc9da0f58f0345fca9d4701baca46361dc934
patiphan01 <86480648+paan1@users.noreply.github.com>
2021-09-05 02:10:50 +0700

    .

--- weekly_4_1.c
+++ weekly_4_1.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
-int main()
+long int main()
 {
     int  num1,num2,num3,x,y,z;
-     scanf("%d %d %d",&num1,&num2,&num3);
+     scanf("%ld %ld %ld",&num1,&num2,&num3);
     x=num1+num2;
     y=num1+num3;
 	  z=num2+num3;
       if (x >= y && x >= z) 
       {
-        printf("%d + %d = %d",num1,num2,x);
+        printf("%ld + %ld = %ld",num1,num2,x);
       } 
       else if (y >= x && y >= z) 
       {
-        printf("%d + %d = %d",num1,num3,y);
+        printf("%ld + %ld = %ld",num1,num3,y);
       } 
       else if (z >= y && z >= x)
       { 
-        printf("%d + %d = %d",num2,num3,z);
+        printf("%ld + %ld = %ld",num2,num3,z);
       }
     return 0;
 }
--- weekly_4_2.c
+++ weekly_4_2.c
@@ -1,22 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int  x,y,z,sum;
-     scanf("%d %d %d",&x,&y,&z);
+    long int  x,y,z,sum;
+     scanf("%ld %ld %ld",&x,&y,&z);
       if (z <= x && z <= y) 
       {
        z=0;
-       printf("%d + %d = ",x,y);
+       printf("%ld + %ld = ",x,y);
       } 
       else if (y <= z && y <= x) 
       {
         y=0;
+        printf("%ld + %ld = ",z,x);
       } 
       else if (x <= y && x <= z)
       { 
         x=0;
+        printf("%ld + %ld = ",y,z);
       }
       sum = x+y+z;
-      printf("%d",sum);
+      printf("%ld",sum);
     return 0;
 }
